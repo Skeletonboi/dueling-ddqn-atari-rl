06a9cccccd0c32d85becc2ac2129562825a6e575
Skeletonboi <victor.yip@mail.utoronto.ca>
2023-03-13 17:16:20 -0700

    exp processing

--- utils/replay_buffers.py
+++ utils/replay_buffers.py
@@ -12,10 +12,20 @@ class ExperienceReplay():
     def insert(self, state, next_state, action, reward, done):
         self.buffer.append((state, next_state, action, reward, done))
 
-    def sample_experience(self, batch_size):    
+    def sample_experience(self, batch_size, device):    
         idxs = np.random.choice(min(batch_size, len(self.buffer)), batch_size)
-        batch_s, batch_ns, batch_a, batch_r, batch_done = zip(*[self.buffer[i] for i in idxs])
-        return batch_s, batch_ns, batch_a, batch_r, batch_done
+        batch_s, batch_ns, batch_a, batch_r, batch_d = zip(*[self.buffer[i] for i in idxs])
+
+        batch_s = torch.tensor(batch_s).to(device)
+        batch_ns = torch.tensor(batch_ns).to(device)
+        batch_a = self.process_vec(batch_a).to(device)
+        batch_r = self.process_vec(batch_r).to(device)
+        batch_d = self.process_vec(batch_d).to(device)
+
+        return batch_s, batch_ns, batch_a, batch_r, batch_d
+
+    def process_vec(self, vec):
+        return torch.tensor(vec).unsqueeze(0).t().long()
 
 class PrioritizedExperienceReplay():
     def __init__(self):
