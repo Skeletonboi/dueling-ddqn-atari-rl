7a62f0ac6f49ad92e913c543f2b1028fe03e3e7a
Skeletonboi <victor.yip@mail.utoronto.ca>
2023-03-13 23:30:15 -0700

    successfully training dqn

--- dqn/main.py
+++ dqn/main.py
@@ -37,35 +37,38 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     device='cpu'
     # Hyperparameters
-    N_EPOCH = 50
+    N_EPOCH = 5000
     N_STEPS = 200
-    UPDATE_STEPS = 10
+    UPDATE_STEPS = 4
 
     N_ENVS = 16
     BATCH_SIZE = 16
     BUFFER_SIZE = 50000
 
     GAMMA = 0.99
-    LR = 0.001
-    EPS = 0.5
-    EPS_DECAY = 0.99
+    LR = 0.0001
+    INIT_EPS = 0.1
+    FIN_EPS = 0.0001
+    EXPLORE = 20000
+    epsilon = INIT_EPS
     # Initialize env
     env = gym.make('CartPole-v1')
     # env = envpool.make('CartPole-v1', env_type='gym', num_envs=N_ENVS)
     n_states = env.observation_space.shape[0]
     n_actions = env.action_space.n
     # Initialize online and target q-networks and exp. replay buffer
-    online_qnet = DQN(fc_size_list=[n_states, 64, n_actions], activation=nn.ReLU(), lr=LR, loss_func=nn.MSELoss()).to(device)
-    target_qnet = DQN(fc_size_list=[n_states, 64, n_actions], activation=nn.ReLU(), lr=LR, loss_func=nn.MSELoss()).to(device)
+    online_qnet = DQN(fc_size_list=[n_states, 64, 256, n_actions], activation=nn.ReLU(), lr=LR, loss_func=nn.MSELoss()).to(device)
+    target_qnet = DQN(fc_size_list=[n_states, 64, 256, n_actions], activation=nn.ReLU(), lr=LR, loss_func=nn.MSELoss()).to(device)
+    target_qnet.load_state_dict(online_qnet.state_dict())
     exp_replay = ExperienceReplay(max_buffer_size=BUFFER_SIZE)
     # Main loop
-    for i in tqdm(range(N_EPOCH)):
+    for epoch in tqdm(range(N_EPOCH)):
         # Train one episode
         s, info = env.reset()
         eps_rew = 0
         for i in range(N_STEPS):
             # eps-greedy action sampling
-            if np.random.uniform(0,1) < EPS:
+            if np.random.uniform(0,1) < epsilon:
                 a = np.random.choice(n_actions)
             else:
                 a = torch.argmax(online_qnet(torch.from_numpy(s).to(device))).item()
@@ -77,20 +80,25 @@ def main():
                 # update target net
                 if i % UPDATE_STEPS == 0:
                     target_qnet.load_state_dict(online_qnet.state_dict())
-                # with torch.no_grad():
+                
                 q_pred = online_qnet.forward(batch_s)
-                q_targ_ns = target_qnet.forward(batch_ns)
-                max_a_ns = torch.argmax(q_targ_ns, dim=1, keepdim=True)
-                q_targ = torch.add(batch_r, GAMMA * (1 - batch_d) * q_targ_ns.gather(dim=1, index=max_a_ns))
+                with torch.no_grad():
+                    q_targ_ns = target_qnet.forward(batch_ns)
+                    q_targ = torch.add(batch_r, GAMMA * (1 - batch_d) * torch.max(q_targ_ns, dim=1, keepdim=True)[0])
 
                 loss = online_qnet.loss_func(q_pred.gather(dim=1, index=batch_a), q_targ).to(device)
                 online_qnet.optimizer.zero_grad()
                 loss.backward()
                 online_qnet.optimizer.step()
+
+                s = next_s
             eps_rew += rew
-        print('Eps. Rew.:', eps_rew)
-        EPS = EPS * EPS_DECAY
-        print('NEW EPS:', EPS)
+            if done:
+                break
+            if epsilon > FIN_EPS:
+                epsilon -= (INIT_EPS - FIN_EPS) / EXPLORE
+        if epoch % 10 == 0:
+            print('Eps. Rew.:', eps_rew)
 
 
 
