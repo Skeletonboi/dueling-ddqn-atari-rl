d52d5379d18d0e8aec64a5d50bbdf70b987e6975
Skeletonboi <victor.yip@mail.utoronto.ca>
2023-03-14 23:16:13 -0700

    sped up exp replay buffer to use lists for O(1) index instead of deque which is O(N)

--- utils/replay_buffers.py
+++ utils/replay_buffers.py
@@ -3,25 +3,35 @@ import numpy as np
 from collections import deque
 
 class ExperienceReplay():
-    def __init__(self, max_buffer_size):
-        self.buffer = deque(maxlen=max_buffer_size)
-    
-    def size(self):
-        return len(self.buffer)
+    def __init__(self, max_buffer_size, n_states, n_actions):
+        self.max_buffer_size = max_buffer_size
+        self.counter = 0
+
+        self.state_mem = np.zeros((max_buffer_size, n_states), dtype=np.float32)
+        self.nstate_mem = np.zeros((max_buffer_size, n_states), dtype=np.float32)
+        self.action_mem = np.zeros(max_buffer_size, dtype=np.float32)
+        self.reward_mem = np.zeros(max_buffer_size, dtype=np.float32)
+        self.done_mem = np.zeros(max_buffer_size, dtype=np.float32)
 
     def insert(self, state, next_state, action, reward, done):
-        self.buffer.append((state, next_state, action, reward, done))
+        idx = self.counter % self.max_buffer_size
+        
+        self.state_mem[idx] = state
+        self.nstate_mem[idx] = next_state
+        self.action_mem[idx] = action
+        self.reward_mem[idx] = reward
+        self.done_mem[idx] = done
 
+        self.counter += 1
     def sample_experience(self, batch_size, device):    
-        idxs = np.random.choice(self.size(), batch_size)
-        batch_s, batch_ns, batch_a, batch_r, batch_d = zip(*[self.buffer[i] for i in idxs])
+        idxs = np.random.choice(min(self.counter, self.max_buffer_size), batch_size)
 
-        batch_s = torch.tensor(batch_s).to(device)
-        batch_ns = torch.tensor(batch_ns).to(device)
-        batch_a = self.process_vec(batch_a).to(device)
-        batch_r = self.process_vec(batch_r).to(device)
-        batch_d = self.process_vec(batch_d).to(device)
-
+        batch_s = torch.tensor(self.state_mem[idxs]).to(device)
+        batch_ns = torch.tensor(self.nstate_mem[idxs]).to(device)
+        batch_a = self.process_vec(self.action_mem[idxs]).to(device)
+        batch_r = self.process_vec(self.reward_mem[idxs]).to(device)
+        batch_d = self.process_vec(self.done_mem[idxs]).to(device)
+        
         return batch_s, batch_ns, batch_a, batch_r, batch_d
 
     def process_vec(self, vec):
