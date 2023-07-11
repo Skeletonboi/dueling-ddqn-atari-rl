cb8dcbe24f6176be8955be51163f32d9e1625749
Skeletonboi <victor.yip@mail.utoronto.ca>
2023-07-11 01:18:24 -0700

    include atari storage option

--- utils/replay_buffers.py
+++ utils/replay_buffers.py
@@ -1,14 +1,17 @@
 import torch
 import numpy as np
-from collections import deque
 
 class ExperienceReplay():
-    def __init__(self, max_buffer_size, n_states, n_actions):
+    def __init__(self, max_buffer_size, n_states, is_atari=False):
         self.max_buffer_size = max_buffer_size
         self.counter = 0
 
-        self.state_mem = np.zeros((max_buffer_size, n_states), dtype=np.float32)
-        self.nstate_mem = np.zeros((max_buffer_size, n_states), dtype=np.float32)
+        if is_atari:
+            self.state_mem = np.zeros((max_buffer_size, ) + n_states, dtype=np.float32)
+            self.nstate_mem = np.zeros((max_buffer_size, ) + n_states, dtype=np.float32)
+        else:
+            self.state_mem = np.zeros((max_buffer_size, n_states), dtype=np.float32)
+            self.nstate_mem = np.zeros((max_buffer_size, n_states), dtype=np.float32)
         self.action_mem = np.zeros(max_buffer_size, dtype=np.float32)
         self.reward_mem = np.zeros(max_buffer_size, dtype=np.float32)
         self.done_mem = np.zeros(max_buffer_size, dtype=np.float32)
@@ -23,6 +26,7 @@ class ExperienceReplay():
         self.done_mem[idx] = done
 
         self.counter += 1
+        
     def sample_experience(self, batch_size, device):    
         idxs = np.random.choice(min(self.counter, self.max_buffer_size), batch_size)
 
